3e6531f74934fde5c73e7e0aa4d201589a68eeeb
PC Fordelone <pc@fordelone.com.br>
2015-06-23 01:06:34 -0300

    - Iniciando o módulo de Symfony - Módulo Symfony - Projeto 01

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on June 23, 2015, 5:32 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/{carro}/{modelo}", name="projeto01")
+     */
+    public function runAction($carro, $modelo)
+    {
+        return $this->render('AppBundle:Default:projeto01.html.twig', ['carro'=>$carro, 'modelo'=>$modelo]);
+    }
+}
